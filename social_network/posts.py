8be6d874ba3d0f86dc06fe6a1ac7efb17b5db466
Abayomi <kabayomi007@gmail.com>
2017-03-11 17:02:55 +0000

    Abayomi, Manny, Fadoua

--- social_network/posts.py
+++ social_network/posts.py
@@ -1,33 +1,46 @@
 from datetime import datetime
 
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        self.text = text
+        self.timestamp = timestamp
+        self.user = None
+
+    def set_user(self, user):
+        self.user = user
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class TextPost(Post):  # Inherit properly     
+    def __init__(self, text, timestamp=None):
+        Post.__init__(self,text,timestamp)
+
+    def __str__(self):
+         #'@Kevin Watson: "Sample post text"\n\tTuesday, Jan 10, 2017' 2017-01-10 00:00:00
+        return '@{} {}: "{}"\n\t{}'.format(self.user.first_name, 
+        self.user.last_name, self.text, self.timestamp.strftime("%A, %b %d, %Y"))
+    
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
 
+class PicturePost(Post):  # Inherit properly
+    def __init__(self, text, image_url, timestamp=None):
+        Post.__init__(self,text,timestamp)
+        self.image_url = image_url
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+    def __str__(self):
+        #'@Kevin Watson: "Sample post text"\n\thttp://fake-domain.com/images/sample.jpg\n\tTuesday, Jan 10, 2017'
+        return '@{} {}: "{}"\n\t{}\n\t{}'.format(self.user.first_name,self.user.last_name,
+        self.text,self.image_url,self.timestamp.strftime("%A, %b %d, %Y"))
+    
+
+class CheckInPost(Post):  # Inherit properly
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        Post.__init__(self,text,timestamp)
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __str__(self):
+        #'@Kevin Checked In: "Sample post text"\n\t-34.603722, -58.381592\n\tTuesday, Jan 10, 2017'
+        return '@{} Checked In: "{}"\n\t{}, {}\n\t{}'.format(self.user.first_name,
+        self.text, self.latitude,self.longitude, self.timestamp.strftime("%A, %b %d, %Y"))
